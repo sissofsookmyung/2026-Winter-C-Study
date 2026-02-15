e70dde6bfc3aba47f0320ce605b1fd846e36101c
La-hee <haha4886@naver.com>
2026-02-15 23:37:21 +0900

    6주차_C언어_26기_김라희

---
+++ 11722.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	int size;
+	scanf("%d", &size);
+
+	//DP를 저장할 배열 생성 및 초기화
+	int* dp = (int*)malloc(sizeof(int) * size);
+	for (int i = 0; i < size; i++) {
+		dp[i] = 1;
+	}
+
+	//입력받은 한 줄을 나눠 배열에 저장
+	int* arr = (int*)malloc(sizeof(int) * size);
+	int n = 0;
+	while (n < size && scanf("%d", &arr[n]) == 1) {
+		n++;
+	}
+
+	//DP를 돌려서 각 숫자로 끝날 때 가질 수 있는 최대 길이의 subset 구하기
+	for (int i = 1; i < size; i++) {
+		for (int find = 0; find < i; find++) {
+			if (arr[i] < arr[find] && dp[i] < dp[find] + 1) {
+				dp[i] = dp[find] + 1;
+			}
+		}
+	}
+
+	//DP 배열에 존재하는 가장 큰 수(가장 긴 감소하는 부분 수열의 길이) 찾기
+	int result = -9999;
+	for (int i = 0; i < size; i++) {
+		if (dp[i] > result)
+			result = dp[i];
+	}
+
+	printf("%d", result);
+
+}
